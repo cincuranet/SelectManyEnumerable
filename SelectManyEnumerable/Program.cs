728eb18045b8faa9ce1499c627a0bfab4d6dfcd6
Jiri Cincura <jiri@x2develop.com>
2014-05-29 10:50:30 +0200

    Implemented "Select".

---
+++ SelectManyEnumerable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectManyEnumerable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Enumerable.Empty<int>();
+		}
+	}
+
+	static class SelectManyEnumerable
+	{
+		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+		{
+			return source.SelectMany(x => new[] { selector(x) });
+		}
+
+		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+		{
+			return source.SelectMany((x, i) => new[] { selector(x, i) });
+		}
+	}
+}
