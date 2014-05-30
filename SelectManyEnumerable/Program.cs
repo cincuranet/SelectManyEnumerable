3a5bb65de084f8b2b0dc1797c08cc148ff641921
Jiri Cincura <jiri@x2develop.com>
2014-05-30 06:45:18 +0200

    Extracted to separate file

--- SelectManyEnumerable/Program.cs
+++ SelectManyEnumerable/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SelectManyEnumerable
 {
@@ -11,19 +8,6 @@ namespace SelectManyEnumerable
 		static void Main(string[] args)
 		{
 			Enumerable.Empty<int>();
-		}
-	}
-
-	static class SelectManyEnumerable
-	{
-		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
-		{
-			return source.SelectMany(x => new[] { selector(x) });
-		}
-
-		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
-		{
-			return source.SelectMany((x, i) => new[] { selector(x, i) });
 		}
 	}
 }
---
+++ SelectManyEnumerable/SelectManyEnumerable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectManyEnumerable
+{
+	static class SelectManyEnumerable
+	{
+		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+		{
+			return source.SelectMany(x => new[] { selector(x) });
+		}
+
+		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+		{
+			return source.SelectMany((x, i) => new[] { selector(x, i) });
+		}
+	}
+}
