25695e48ded1568aff1d3c03fbd32a7d7c4efc58
Jiri Cincura <jiri@x2develop.com>
2014-05-30 07:05:49 +0200

    Added tests.

---
+++ SelectManyEnumerable.Tests/SelectTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SelectManyEnumerable.Tests
+{
+	[TestFixture]
+	public class SelectTests : TestsBase
+	{
+		[Test]
+		public void SelectTests_Basic()
+		{
+			var data = GetData();
+			var expected = Enumerable.Select(data, x => x * x);
+			var current = SelectManyEnumerable.Select(data, x => x * x);
+			CollectionAssert.AreEqual(expected, current);
+		}
+
+		[Test]
+		public void SelectTests_BasicWithIndex()
+		{
+			var data = GetData();
+			var expected = Enumerable.Select(data, (x, i) => x + i);
+			var current = SelectManyEnumerable.Select(data, (x, i) => x + i);
+			CollectionAssert.AreEqual(expected, current);
+		}
+	}
+}
---
+++ SelectManyEnumerable.Tests/TestsBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectManyEnumerable.Tests
+{
+	public class TestsBase
+	{
+		protected static int[] GetData()
+		{
+			return Enumerable.Range(1, 128).ToArray();
+		}
+	}
+}
