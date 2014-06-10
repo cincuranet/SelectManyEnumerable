778b4ecebb2ec2bba172e3d9d80bc68937a6c546
Jiri Cincura <jiri@x2develop.com>
2014-06-10 15:02:21 +0200

    Implemeted as stacking simpler functions.

--- SelectManyEnumerable/SelectManyEnumerable.cs
+++ SelectManyEnumerable/SelectManyEnumerable.cs
@@ -8,9 +8,8 @@ namespace SelectManyEnumerable
 	{
 		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
 		{
-			return source.SelectMany(x => new[] { selector(x) });
+			return Select(source, (x, _) => selector(x));
 		}
-
 		public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
 		{
 			return source.SelectMany((x, i) => new[] { selector(x, i) });
@@ -18,9 +17,8 @@ namespace SelectManyEnumerable
 
 		public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
 		{
-			return source.SelectMany(x => predicate(x) ? new[] { x } : new TSource[0]);
+			return Where(source, (x, _) => predicate(x));
 		}
-
 		public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
 		{
 			return source.SelectMany((x, i) => predicate(x, i) ? new[] { x } : new TSource[0]);
